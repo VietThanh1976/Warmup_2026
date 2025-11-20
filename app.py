77a5de81c39ca1d340c9c42198ddff38bcb054ce
VietThanh1976 <165611983+VietThanh1976@users.noreply.github.com>
2025-11-20 22:01:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
 
-st.write('Hello')
-st.write(55)
-st.divider()
-file = st.file_uploader(
-  "Upload your file",
-  type=["pdf", "docx", "wav"]
-)
-if file:
-  st.write(f"{file}")
+def fact(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n*fact(n-1)
+  
+def main():
+  st.title("Tính Giai Thừa")
+  st.number_input(
+    "Nhập vào một số:",
+    min_value = 0,
+    max_value = 200
+  )
+  if st.button("Tính toán"):
+    result = fact(number)
+    st.write(f"Gia thua của {number} la {result}")
+if __name__=="__main__":
+  main()
+  
