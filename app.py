ac0249579320c74956fda16870be6a55cd0c097d
VietThanh1976 <165611983+VietThanh1976@users.noreply.github.com>
2025-11-21 10:34:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import soundfile as sf
 from streamlit_mic_recorder import mic_recorder
 import io
 import os
-#from docx import Document # Thư viện cho file .docx
-import docx
-import Document
+from docx import Document # Thư viện cho file .docx
 import time
 
 # =========================================================================
